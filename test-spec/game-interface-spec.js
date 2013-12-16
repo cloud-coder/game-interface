f9eb8f46b3b49cabd010cc37efb62ef24e438788
Sean Smith <dremoor@gmail.com>
2013-12-15 19:16:12 -0500

    test harness done

--- test-spec/game-interface-spec.js
+++ test-spec/game-interface-spec.js
@@ -8,7 +8,9 @@ test.heading('Library', function () {
 });
 test.heading('Classes', function () {
   test.paragraph('These objects make up the core "classes" and are extended via javascript prototype inheritance.');
-  // test.runnerAttribute();
+  test.runnerGameState();
+  test.runnerViewPort();
+  test.runnerGraphic();
 });
 test.heading('Models', function () {
   test.paragraph('These core models are included in the library.');
